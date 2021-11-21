ff893577899ce9ed3dd4599e7fc006485e1ab336
Ren0503 <illuminati0440@gmail.com>
2021-11-21 14:43:12 +0700

    Change style

---
+++ client/src/styles/theme.ts
@@ -0,0 +1,51 @@
+export const theme = {
+    colors: {
+        background: "#282829",
+        primary: "#ffffff",
+        secondary: "0a0a0a",
+        red: "f43547",
+        grey: "#202020",
+        black: "#121212",
+        blue: "#3EA6FF",
+        pink: "#CD113B",
+        purple: "#52006A",
+    },
+    font: {
+        family: "'Open Sans', sans-serif",
+        weight: {
+            light: '300',
+            normal: '400',
+            bold: '600',
+        },
+        size: {
+            tiny: '11px',
+            xxs: '13px',
+            xs: '14px',
+            sm: '16px',
+            md: '18px',
+            lg: '20px',
+            xl: '34px',
+        },
+    },
+    spacing: {
+        none: 0,
+        xxs: '5px',
+        xs: '10px',
+        sm: '20px',
+        md: '30px',
+        lg: '40px',
+        xl: '60px',
+    },
+    radius: {
+        sm: '5px',
+        md: '10px',
+        lg: '15px',
+    },
+    zIndex: {
+        xs: 10,
+        sm: 20,
+        md: 30,
+        lg: 40,
+        xl: 50,
+    }
+};
